6fb6f046acc62baea579221ba1cc85a37147f84c
Vinicius <agenciaprocurada@gmail.com>
2026-03-20 20:46:36 -0300

    add Microsoft Clarity tracking (vyyikebv84)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,13 @@ export default async function RootLayout({
       className={`${playfair.variable} ${inter.variable} ${cormorant.variable}`}
     >
       <body className="min-h-screen flex flex-col text-text-primary">
+        <Script id="clarity-init" strategy="afterInteractive">{`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vyyikebv84");
+        `}</Script>
         <Script src="https://www.googletagmanager.com/gtag/js?id=AW-18030402125" strategy="afterInteractive" />
         <Script id="gtag-init" strategy="afterInteractive">{`
           window.dataLayer = window.dataLayer || [];
