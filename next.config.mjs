4109a2984221b754cff2fbfd68be3c8c0676bef0
Vinicius <agenciaprocurada@gmail.com>
2026-03-20 10:13:14 -0300

    redirect index

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://portal.sabedoriamistica.com.br",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
